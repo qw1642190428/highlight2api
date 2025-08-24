6903a1f301d3569f4cab13c8f11a576e07ebe492
jhhgiyv <155544499+jhhgiyv@users.noreply.github.com>
2025-08-24 15:03:42 +0800

    fix: 环境变量bool

--- app/config.py
+++ app/config.py
@@ -9,8 +9,8 @@ HIGHLIGHT_BASE_URL = "https://chat-backend.highlightai.com"
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Highlight/1.3.61 Chrome/132.0.6834.210 Electron/34.5.8 Safari/537.36"
 
 # 网络配置
-TLS_VERIFY = os.environ.get("TLS_VERIFY", True)
-DEBUG = os.environ.get("DEBUG", False)
+TLS_VERIFY = os.environ.get("TLS_VERIFY", 'True').lower() == "true"
+DEBUG = os.environ.get("DEBUG", 'False').lower() == "true"
 
 if not DEBUG:
     logger.remove()
